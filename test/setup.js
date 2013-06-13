75467b65cc8ced1e6bc4aecd5387ee6e79e15052
Rico Sta. Cruz <hi@ricostacruz.com>
2013-06-14 04:34:19 +0800

    Use support/env for testing.

--- test/setup.js
+++ test/setup.js
@@ -3,43 +3,12 @@ global.extend = require('util')._extend;
 global.chai = require('chai');
 chai.should();
 
-// Helpers
-global.testSuite = testSuite;
+var env = require('./support/env');
+global.testSuite = env.suite(['1.7', '1.8', '1.9', '1.10', '2.0'], myEnv);
 
-var jsdom = require('jsdom');
-
-function testSuite(name, fn) {
-  ['1.7', '1.8', '1.9', '1.10', '2.0'].forEach(function(jq) {
-    describe('jQuery '+jq+' '+name, function() {
-      beforeEach(env(jq));
-      fn.apply(this, []);
-    });
+function myEnv(jq) {
+  return env({
+    expose: ['jQuery', '$'],
+    js: [ 'vendor/jquery-'+jq+'.js', 'simpletap.js' ]
   });
-}
-
-function env(jq) {
-  return function(done) {
-    jsdom.env({
-      html: '',
-      src: [
-        getFile('../vendor/jquery-'+jq+'.js'),
-        getFile('../simpletap.js')
-      ],
-      done: function(errors, window) {
-        window.console = console;
-        extend(global, {
-          jQuery : window.jQuery,
-          $      : window.$,
-          window : window
-        });
-        done(errors);
-      }
-    });
-  };
-}
-
-function getFile(filepath) {
-  var path = require('path');
-  var fs = require('fs');
-  return require('fs').readFileSync(path.resolve(__dirname, filepath)).toString();
 }
