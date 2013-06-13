81887d5edab6e97fdf88878d885fd60cc859b25b
Rico Sta. Cruz <hi@ricostacruz.com>
2013-06-14 04:50:27 +0800

    Simplify.

--- test/setup.js
+++ test/setup.js
@@ -2,12 +2,31 @@
 global.chai = require('chai');
 chai.should();
 
-var env = require('./support/env');
-global.testSuite = env.suite(['1.7', '1.8', '1.9', '1.10', '2.0'], myEnv);
+var fs = require('fs');
+var multisuite = require('./support/multisuite');
+global.testSuite = multisuite(['jq-1.7', 'jq-1.8', 'jq-1.9', 'jq-1.10', 'jq-2.0'], myEnv);
+
+var scripts = {
+  'jq-1.7':  fs.readFileSync('vendor/jquery-1.7.js'),
+  'jq-1.8':  fs.readFileSync('vendor/jquery-1.8.js'),
+  'jq-1.9':  fs.readFileSync('vendor/jquery-1.9.js'),
+  'jq-1.10': fs.readFileSync('vendor/jquery-1.10.js'),
+  'jq-2.0':  fs.readFileSync('vendor/jquery-2.0.js'),
+  'simpletap': fs.readFileSync('simpletap.js')
+};
 
 function myEnv(jq) {
-  return env({
-    expose: ['jQuery', '$'],
-    js: [ 'vendor/jquery-'+jq+'.js', 'simpletap.js' ]
-  });
+  var jsdom = require('jsdom');
+  return function(done) {
+    jsdom.env({
+      html: '<!doctype html><html><head></head><body></body></html>',
+      src: [ scripts[jq], scripts.simpletap ],
+      done: function(errors, window) {
+        window.console = console;
+        global.window  = window;
+        global.$       = window.$;
+        done(errors);
+      }
+    });
+  };
 }
